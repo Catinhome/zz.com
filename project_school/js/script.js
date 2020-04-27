4c49e997c05269b3a448cc4ce260a78b40ed873d
Catinhome <60307715+Catinhome@users.noreply.github.com>
2020-04-28 00:49:03 +0900

    Delete script.js

--- project_school/js/script.js
+++
@@ -1,7 +0,0 @@
-//Этот код добавляет классы к другим классам
-$(document).ready(function() {
-	$('.header__burger').click(function(event) {
-      $('.header__burger,.header__menu').toggleClass('active');
-      $('body').toggleClass('lock');
-	});
-});
